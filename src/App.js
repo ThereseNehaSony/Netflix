680f55f19f1af350a9c18cb150f3adb35ac17385
Therese Neha Sony <nehakarickal100@gmail.com>
2024-04-06 12:11:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route } from 'react-router-dom'
+import NavBar from "./Components/NavBar";
+import Home from './Pages/Home'
+import { AuthContextProvider } from './Context/AuthContext';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Account from './Pages/Account';
+import ProtectedRoute from './Components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <AuthContextProvider>
+    <NavBar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/account' element={ <ProtectedRoute > <Account /> </ProtectedRoute> } />
+    
+    </Routes>
+    </AuthContextProvider>
+  </>
   );
 }
 
---
+++ src/Components/Row.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Movie from './Movie'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+const Row = ({title,fetchUrl, rowID}) => {
+    const [movies,setMovies] = useState([])
+    
+
+    useEffect (()=>{
+      axios.get(fetchUrl).then((response)=>{
+        setMovies(response.data.results)
+      })
+    },[fetchUrl])
+ 
+    const slideLeft=()=>{
+        let slider = document.getElementById('slider' + rowID)
+        slider.scrollTo({
+            left: slider.scrollLeft - 500,
+            behavior: 'smooth'
+        });
+    }
+    const slideRight=()=>{
+        let slider = document.getElementById('slider' + rowID)
+        slider.scrollTo({
+            left: slider.scrollLeft + 500,
+            behavior: 'smooth'
+        });
+    }
+
+  return (
+    <>
+      <h2 className='text-white font-bold md:text-xl p-4 ' >{title}</h2>
+      <div className='relative flex items-center group '>
+        < MdChevronLeft
+        onClick={slideLeft}
+         className='bg-white rounded-full left-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+        
+        <div id={'slider' + rowID } className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smoth scrollbar-hide' >
+            {movies.map((item,id)=>(
+               <Movie key={id} item={item} /> 
+
+            ))}
+        </div>
+        < MdChevronRight
+        onClick={slideRight}
+         className='bg-white rounded-full right-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+      </div>
+
+
+    </>
+  )
+}
+
+export default Row
+
